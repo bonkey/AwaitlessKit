7a632180dd2ce210f32db58fc449b694525a8291
Daniel Bauke <daniel@bauke.pl>
2025-04-27 13:04:17 +0200

    Add compiler version check for withCompletion function

--- Sources/AwaitlessKit/Noasync.withCompletion.swift
+++ Sources/AwaitlessKit/Noasync.withCompletion.swift
@@ -5,35 +5,37 @@
 import Foundation
 
 extension Noasync {
-    public static func withCompletion(
-        _ code: sending () async throws(Failure) -> Success,
-        _ completion: @Sendable @escaping (Result<Success, Failure>) -> Void)
-    {
-        let semaphore = DispatchSemaphore(value: 0)
+    #if compiler(>=6.0)
+        public static func withCompletion(
+            _ code: sending () async throws(Failure) -> Success,
+            _ completion: @Sendable @escaping (Result<Success, Failure>) -> Void)
+        {
+            let semaphore = DispatchSemaphore(value: 0)
 
-        nonisolated(unsafe) var result: Result<Success, Failure>?
+            nonisolated(unsafe) var result: Result<Success, Failure>?
 
-        withoutActuallyEscaping(code) {
-            nonisolated(unsafe) let sendableCode = $0
+            withoutActuallyEscaping(code) {
+                nonisolated(unsafe) let sendableCode = $0
 
-            let coreTask = Task<Void, Never>
-                .detached(priority: .userInitiated) { @Sendable () async in
+                let coreTask = Task<Void, Never>
+                    .detached(priority: .userInitiated) { @Sendable () async in
 
-                    do {
-                        result = try await .success(sendableCode())
-                    } catch {
-                        result = .failure(error as! Failure)
+                        do {
+                            result = try await .success(sendableCode())
+                        } catch {
+                            result = .failure(error as! Failure)
+                        }
                     }
+
+                Task<Void, Never>.detached(priority: .userInitiated) {
+                    await coreTask.value
+                    semaphore.signal()
                 }
 
-            Task<Void, Never>.detached(priority: .userInitiated) {
-                await coreTask.value
-                semaphore.signal()
+                semaphore.wait()
             }
 
-            semaphore.wait()
+            completion(result!)
         }
-
-        completion(result!)
-    }
+    #endif
 }
